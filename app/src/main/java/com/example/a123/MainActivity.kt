fe91aeb6867129f7184881aa02accd8e1788c3e9
student <julian.stajnke1@gmail.com>
2022-01-12 11:00:11 +0100

    tego

---
+++ app/src/main/java/com/example/a123/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button2)
+
+    }
+}
